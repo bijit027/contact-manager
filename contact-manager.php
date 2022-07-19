c97e9cbd768c20011f23d82db003aab44632d0aa
bijit027 <debbijit70@gmail.com>
2022-07-19 15:59:03 +0600

    proper spacing

--- contact-manager.php
+++ contact-manager.php
@@ -40,85 +40,85 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 
 final class Contact_Manager
 {
-  /**
-   * Define Plugin Version
-   */
-  const VERSION = '1.0.0';
+    /**
+     * Define Plugin Version
+     */
+    const VERSION = '1.0.0';
 
-  /**
-   * Construct Function
-   */
-  public function __construct()
-  {
-    $this->plugin_constants();
-    register_activation_hook( __FILE__, [$this, 'activate'] );
-    register_deactivation_hook( __FILE__, [$this, 'deactivate'] );
-    add_action( 'plugins_loaded', [$this, 'init_plugin'] );
-  }
-
-  /**
-   * Plugin Constants
-   * @since 1.0.0
-   */
-  public function plugin_constants()
-  {
-    define( 'CM_VERSION', self::VERSION );
-    define( 'CM_PLUGIN_PATH', trailingslashit( plugin_dir_path(__FILE__) ) );
-    define( 'CM_PLUGIN_URL', trailingslashit( plugins_url('', __FILE__) ) );
-    define( 'CM_ASSETS', CM_PLUGIN_URL . '/assets' );
-    define( 'CM_CONTACTS_BASE_DIR', plugin_dir_url( __FILE__ ) );
-    define( 'CM_CONTACTS_PATH', __DIR__ );
-  }
-
-  /**
-   * Singletone Instance
-   * @since 1.0.0
-   */
-  public static function init()
-  {
-    static $instance = false;
-
-    if ( !$instance ) {
-      $instance = new self();
+    /**
+     * Construct Function
+     */
+    public function __construct()
+    {
+        $this->plugin_constants();
+        register_activation_hook( __FILE__, [$this, 'activate'] );
+        register_deactivation_hook( __FILE__, [$this, 'deactivate'] );
+        add_action( 'plugins_loaded', [$this, 'init_plugin'] );
     }
 
-    return $instance;
-  }
-
-  /**
-   * On Plugin Activation
-   * @since 1.0.0
-   */
-  public function activate()
-  {
-    $installer = new CM\Includes\Installer();
-    $installer->run();
-  }
-
-  /**
-   * On Plugin De-actiavtion
-   * @since 1.0.0
-   */
-  public function deactivate()
-  {
-    // On plugin deactivation
-  }
-
-  /**
-   * Init Plugin
-   * @since 1.0.0
-   */
-  public function init_plugin()
-  {
-    new \CM\Includes\Assets();
-    new \CM\Includes\Models();
-    new \CM\Includes\AdminAjaxHandler();
-    if (is_admin()) {
-      new \CM\Includes\Admin();
-    } else {
-      new \CM\Includes\Frontend();
+    /**
+     * Plugin Constants
+     * @since 1.0.0
+     */
+    public function plugin_constants()
+    {
+        define( 'CM_VERSION', self::VERSION );
+        define( 'CM_PLUGIN_PATH', trailingslashit( plugin_dir_path(__FILE__) ) );
+        define( 'CM_PLUGIN_URL', trailingslashit( plugins_url('', __FILE__) ) );
+        define( 'CM_ASSETS', CM_PLUGIN_URL . '/assets' );
+        define( 'CM_CONTACTS_BASE_DIR', plugin_dir_url( __FILE__ ) );
+        define( 'CM_CONTACTS_PATH', __DIR__ );
     }
-  }
+
+    /**
+     * Singletone Instance
+     * @since 1.0.0
+     */
+    public static function init()
+    {
+        static $instance = false;
+
+        if ( !$instance ) {
+            $instance = new self();
+        }
+
+            return $instance;
+    }
+
+    /**
+     * On Plugin Activation
+     * @since 1.0.0
+     */
+    public function activate()
+    {
+        $installer = new CM\Includes\Installer();
+        $installer->run();
+    }
+
+    /**
+     * On Plugin De-actiavtion
+     * @since 1.0.0
+     */
+    public function deactivate()
+    {
+      // On plugin deactivation
+    }
+
+    /**
+     * Init Plugin
+     * @since 1.0.0
+     */
+    public function init_plugin()
+    {
+        new \CM\Includes\Assets();
+        new \CM\Includes\Models();
+        new \CM\Includes\AdminAjaxHandler();
+        if (is_admin()) {
+            new \CM\Includes\Admin();
+        } else {
+            new \CM\Includes\Frontend();
+        }
+    }
 }
 
 /**
@@ -127,7 +127,7 @@ final class Contact_Manager
  */
 function Contact_Manger()
 {
-  return Contact_Manager::init();
+    return Contact_Manager::init();
 }
 
 // Run the Plugin
